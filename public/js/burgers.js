dea8626c4b9233a8bbbfa7df7d592bebf5cfb46a
BPresseisen <benpresseisen@gmail.com>
2019-05-13 15:02:37 -0400

    20190513v2

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -2,19 +2,37 @@
 
 // var burgerImages = require("../data/burgerImages");
 
-// var burgerImageID;
+var burgerImageID;
 
 $(function() {
     $(".change-devoured").on("click", function(event) {
 
       console.log ("on the click-event in the burger.js file");
 
+      burgerImageID = ((Math.random())*33).toFixed(0)+1;
+
+      console.log("the burgerImage ID is: " + burgerImageID);
+
       var id = $(this).data("id");
       var newDevoured = $(this).data("newdevoured");
   
+      if (newDevoured===false){
+
+        newDevoured=1
+
+      }else if (newDevoured===true){
+
+         newDevoured=0
+
+      };
+
+      console.log ("the newDevoured is: " +newDevoured);
+
       var newDevouredState = {
         devoured: newDevoured
       };
+
+      console.log("the newDevouredState is: " + newDevouredState);
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
@@ -22,6 +40,11 @@ $(function() {
         data: newDevouredState
       }).then(
         function() {
+
+          burgerImageID = ((Math.random())*33).toFixed(0)+1;
+
+          console.log("the burgerImage ID is: " + burgerImageID);
+
           console.log("changed devoured to", newDevoured);
           // Reload the page to get the updated list
           location.reload();
@@ -31,6 +54,8 @@ $(function() {
     // CHANGE LEAD IMAGE
 
     // burgerImageID = ((Math.random())*33)+1;
+
+    // console.log("the burgerImage ID is: " + burgerImageID);
 
     // function displayImage() {
     //   // $("#leadImage").html("<img src=" + images[count] + " width='400px'>");
@@ -43,12 +68,17 @@ $(function() {
   
     $(".create-form").on("submit", function(event) {
       console.log("hello");
+
+      burgerImageID = ((Math.random())*33).toFixed(0)+1;
+
+      console.log("the burgerImage ID is: " + burgerImageID);
+
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurger = {
-        burger: $("#burger").val().trim()
-        // devoured: false
+        burger: $("#burger").val().trim(),
+        devoured: 0
       };
   
       console.log(newBurger);
@@ -60,13 +90,24 @@ $(function() {
       }).then(
         function() {
           console.log("created new burger");
+
           // Reload the page to get the updated list
           location.reload();
         }
       );
+
+      // burgerImageID = ((Math.random())*33)+1;
+
+      // console.log("the burgerImage ID is: " + burgerImageID);
+
     });
   
     $(".delete-burger").on("click", function(event) {
+
+      burgerImageID = ((Math.random())*33)+1;
+
+      console.log("the burgerImage ID is: " + burgerImageID);
+
       var id = $(this).data("id");
   
       // Send the DELETE request.
@@ -75,10 +116,12 @@ $(function() {
       }).then(
         function() {
           console.log("deleted burger", id);
+
           // Reload the page to get the updated list
           location.reload();
         }
       );
+
     });
   });
   
